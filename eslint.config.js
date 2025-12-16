86e68b5facb4af6c9d8096a95f64a23c355402ce
Salman <salman.nazal2001@gmail.com>
2025-12-16 18:51:33 +0300

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -99,13 +99,13 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from "eslint-plugin-storybook";
 import featureSliced from "@conarti/eslint-plugin-feature-sliced";
-import importPlugin from "eslint-plugin-import";
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
+import importPlugin from "eslint-plugin-import"
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config(
   [
